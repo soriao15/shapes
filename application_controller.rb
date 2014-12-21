d4dbe3f6b961ae10290332681b7e553b4737a5d7
soriao15 <omarlsoria@gmail.com>
2014-12-21 13:05:36 -0500

    added functionality

--- application_controller.rb
+++ application_controller.rb
@@ -1,11 +1,50 @@
 require 'bundler'
 Bundler.require
 require_relative './models/triangle.rb'
+require_relative './models/circle.rb'
+require_relative './models/rectangle.rb'
 
 class MyApp < Sinatra::Base
 
-  get '/home' do
+get '/home' do
   	erb :index
-  
-  end
+end
+ 
+get '/triangle' do	
+  erb :triangle
+end
+
+post '/triangle' do
+	@triangle1= Right_triangle.new
+	@triangle1.leg1= params[:leg1].to_f unless params[:leg1] == ""
+	@triangle1.leg2= params[:leg2].to_f unless params[:leg2] == ""
+	@triangle1.hypotenuse= params[:hypotenuse].to_f unless params[:hypotenuse] == ""
+	@triangle1.angle_1= params[:angle_1].to_f unless params[:angle_1] == ""
+	@triangle1.angle_2= params[:angle_2].to_f unless params[:angle_2] == ""
+	@triangle1.angle_3= params[:angle_3].to_f unless params[:angle_3] == ""
+	@triangle1.area= params[:area].to_f unless params[:area] == ""
+	@triangle1.perimeter= params[:perimeter].to_f unless params[:perimeter] == ""
+	@triangle1.calculate_angles
+	@triangle1.calculate_legs
+	@triangle1.calculate_area
+	@triangle1.calculate_perimeter
+	erb :triangle_results
+end
+ 
+get '/circle' do
+  	erb :circle
+end
+post '/circle' do
+	@circle1=Circle.new
+	@circle1.radius= params[:radius].to_f unless params[:radius] == ""
+	@circle1.diameter= params[:diameter].to_f unless params[:diameter] == ""
+	@circle1.circumfrence= params[:circumfrence].to_f unless params[:circumfrence] == ""
+	@circle1.area= params[:area].to_f unless params[:area] == ""
+	@circle1.calculate_area
+	@circle1.calculate_circumfrence
+	@circle1.diameter
+	@circle1.calculate_radius
+	erb :circle_results
+
+end	
 end
---
+++ models/rectangle.rb
@@ -0,0 +1,33 @@
+class Rectangle
+def length=(l)
+	@l = l
+end
+def length
+	@l
+end
+def width=(w)
+	@w = w1
+end
+def width
+	@w
+end
+def area(a)
+	@a = a
+end
+def area
+	@a
+end
+def perimeter=(p)
+	@p = p
+end
+def perimeter
+	@p
+end
+def calculate_area
+	@a = @l * @w
+end
+def calculate_perimeter
+	@p = (2*@l) + (2*@w)
+end
+end
+
